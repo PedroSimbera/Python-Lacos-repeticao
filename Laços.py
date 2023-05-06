421d25bbc4efd38ae3705d365446f50185cdf095
PedroSimbera <pedrosimbera@hotmail.com>
2023-05-06 15:08:17 -0300

    sobre laços while e for

---
+++ Laços.py
@@ -0,0 +1,14 @@
+#while
+a = int(input("Digite um valor: "))
+
+while a < 10:
+    print("Menor que o 10")
+
+#É um exemplo, mas isso gera um loop infinito, pois não há incremento que fará parar essa execução
+#Geralmente é solicitado uma ação, se essa ação for verdadeira terá a ação do while, e dentro do while ...
+#tem uma condição com algum outro comando, por exemplo, enquanto o número for menor que outro, solicite ...
+#uma nova entrada / ou enquanto for diferente de algum valor previsto, solicite novas entradas, e poderá ...
+#somar as entradas, por exemplo, ou depois mostrá-las.
+
+
+#FOR
---
+++ SobreLaçoRepeticao.py
@@ -0,0 +1,9 @@
+#A estrutura que se define se repete em laço enquanto tiver ocorrendo / sendo verdadeiro a condiçao
+#Um loop - com o While (Enquanto)
+#Enquanto for verdadeiro faça
+#Assim, o while é usando quando não se sabe a quantidade de repetição precisará ser realizada
+
+
+#Com o FOR, sabe-se quantas vezes será necessário repetir o laço, pois tem um "in range" ...
+#um até que aconteça algo, alcance determinado limite pre-estabelecido ou entrada pelo usuário...
+#Além disso tem um incremento, que fará com que a cada loop o valor mude
