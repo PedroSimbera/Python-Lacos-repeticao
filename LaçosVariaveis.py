a8ce3247872060e24c4b80054357d59ad6f74f25
PedroSimbera <pedrosimbera@hotmail.com>
2023-05-06 19:58:41 -0300

    interação dos laços de repetição com vetores

--- LaçosVariaveis.py
+++ LaçosVariaveis.py
@@ -12,3 +12,16 @@
 # b = n - sendo n o valor entrada pelo usuário informando quantas vezes serão dadas entradas)..
 # Com isso utiliza o "append" que em python estamos dizendo que será dada uma nova entrada a ser inserida ao...
 #da lista que estamos criando e n elementos.
+
+
+#Como estamos trabalhando com lista, podemos chamar posições específicas da lista
+#Começando do 0, até o último elemento. 
+#Para chamaar o elemento zero será print(x[0])
+
+# a=["Pedro","Joao"]
+# print(a[0]) -> Pedro
+
+#Para inserir um novo elemento e na posiçao é "x.insert(1, "Jose")" Isso fará a inserção de jose...
+#antes de joão e joao ficará na posição 2.
+
+#Para remover é o metodo "x.remove" e apontar a posição ou elemento.
