32bed8e0a0a6b6c9d281d087b8e86b32dea65d56
PedroSimbera <pedrosimbera@hotmail.com>
2023-05-06 15:26:23 -0300

    sobre laços interagindo com varivavel e entrada ao rodar o loop

---
+++ LaçosVariaveis.py
@@ -0,0 +1,14 @@
+# a = []
+# b = 1
+
+# while b <= 4:
+#     a.append(input("Digite um nome: "))
+#     b = b + 1
+
+# print(a)
+
+#Um exemplo de como criar uma lista
+#Precisa ter o incremento neste caso, por não se saber o limite que será digitado (não aqui, mas geralmente é...
+# b = n - sendo n o valor entrada pelo usuário informando quantas vezes serão dadas entradas)..
+# Com isso utiliza o "append" que em python estamos dizendo que será dada uma nova entrada a ser inserida ao...
+#da lista que estamos criando e n elementos.
