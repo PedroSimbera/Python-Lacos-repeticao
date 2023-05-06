23e3dcf1fe639fce4e1a42647d23c11d3e4d93a7
PedroSimbera <pedrosimbera@hotmail.com>
2023-05-06 15:20:28 -0300

    sobre laços for

--- Laços.py
+++ Laços.py
@@ -1,8 +1,8 @@
 #while
-a = int(input("Digite um valor: "))
 
-while a < 10:
-    print("Menor que o 10")
+# a = int(input("Digite um valor: "))
+# while a < 10:
+#     print("Menor que o 10")
 
 #É um exemplo, mas isso gera um loop infinito, pois não há incremento que fará parar essa execução
 #Geralmente é solicitado uma ação, se essa ação for verdadeira terá a ação do while, e dentro do while ...
@@ -12,3 +12,24 @@ while a < 10:
 
 
 #FOR
+
+# for a in range (5):
+#     print("Pedro")
+
+#O uso do for possibilita que utilizemos também vetores, para armazenar todas as entradas dentro de uma lista na variável
+
+# a = ["Pedro", "Jose", "Joao", "Lucas", "Marcio"]
+# for a in a:
+#     print(a)
+
+#Também é possível percorrer naõ com o "in range", mas com o "in", percorrer os elementos dentro da variável
+#Além disso, não é comum que seja acrescentado os elementos dessa forma, é feito a solicitaçaõ para que o usuário...
+#entre com os valores.
+
+# a = "Pedro"
+# for a in a:
+#     print(a)
+
+#Além daquelas opções, tem-se para o python que ao se valer de uma única string na variável, e depois quer...
+#percorre-la, no mínimo está solicitando que percorrar elemento por elemento da string.
+
